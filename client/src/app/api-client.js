c15f4c17928b0a4080ac7ed08261a86d2a3ff395
tanvi kulkarni <tanvi18.kulkarni@gmail.com>
2025-08-01 23:31:40 +0530

    deployvercel push

--- client/src/app/api-client.js
+++ client/src/app/api-client.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_API_URL,
-  credentials: "include",
+  // credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const auth = getState().auth;
     if (auth?.accessToken) {
